fbdfdd89324de33649782b9b8f51d6bd75055155
Joe Badaczewski <joebad@amazon.com>
2022-11-20 23:17:33 -0500

    [Github workflow] turning off image optimizations

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 //next.config.js
-const { withContentlayer } = require('next-contentlayer');
+const {withContentlayer} = require('next-contentlayer');
 
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['images.unsplash.com', 'media-exp1.licdn.com'],
-    dangerouslyAllowSVG: true,
-  }
+    reactStrictMode: true,
+    images: {
+        domains: ['images.unsplash.com', 'media-exp1.licdn.com'],
+        dangerouslyAllowSVG: true,
+        unoptimized: true
+    }
 }
 
 module.exports = withContentlayer(nextConfig)
