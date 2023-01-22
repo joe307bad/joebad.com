0a1813e18316f44d5c010a7f25d7d760727f7707
Joe Badaczewski <joebad@amazon.com>
2023-01-22 15:28:14 -0500

    [deploy] [netlify] troubleshooting

--- next.config.js
+++ next.config.js
@@ -4,19 +4,11 @@ const { withContentlayer } = require("next-contentlayer");
 
 const nextConfig = {
   reactStrictMode: false,
-  exportTrailingSlash: true,
   images: {
     domains: ["images.unsplash.com", "media-exp1.licdn.com"],
     dangerouslyAllowSVG: true,
     unoptimized: true,
-  },
-  webpack: (config) => {
-    // this will override the experiments
-    config.experiments = { ...config.experiments, topLevelAwait: true };
-    // this will just update topLevelAwait property of config.experiments
-    // config.experiments.topLevelAwait = true
-    return config;
-  },
+  }
 };
 
 module.exports = withContentlayer(nextConfig);
