4f310da3cf791e5c3c6560e6b4979bc661780ad0
Joe Badaczewski <joebad@amazon.com>
2023-07-23 23:49:53 -0400

    [build error] adding NextIntlClientProvider to article page

--- pages/article/[slug].js
+++ pages/article/[slug].js
@@ -3,6 +3,7 @@ import { NextSeo } from "next-seo";
 import { SingleArticle } from "../../components/SingleArticle";
 import { useMDXComponent } from "next-contentlayer/hooks";
 import SampleComponent from "../../components/SampleComponent";
+import {NextIntlClientProvider} from "next-intl";
 
 const usedcomponents = {
   SampleComponent,
@@ -11,13 +12,13 @@ const SinglePost = ({ article }) => {
   const MDXContent = useMDXComponent(article.body.code);
 
   return (
-    <>
+    <NextIntlClientProvider locale="en-US">
       <NextSeo title={article.title} description={article.seoDescription} />
 
       <SingleArticle {...article}>
         <MDXContent components={usedcomponents} />
       </SingleArticle>
-    </>
+    </NextIntlClientProvider>
   );
 };
 export default SinglePost;
